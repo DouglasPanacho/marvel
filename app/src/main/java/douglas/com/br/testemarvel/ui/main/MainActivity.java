65fd18fdbbecae4a66a85dc68efa43785d70e5b6
Douglas Panacho <panachodouglas@gmail.com>
2017-12-01 00:19:42 -0200

    created md5 helper

--- app/src/main/java/douglas/com/br/testemarvel/ui/main/MainActivity.java
+++ app/src/main/java/douglas/com/br/testemarvel/ui/main/MainActivity.java
@@ -1,12 +1,15 @@
 package douglas.com.br.testemarvel.ui.main;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import javax.inject.Inject;
 
 import dagger.android.AndroidInjection;
+import douglas.com.br.testemarvel.Constants;
 import douglas.com.br.testemarvel.R;
 import douglas.com.br.testemarvel.ui.base.BaseActivity;
+import douglas.com.br.testemarvel.utils.helpers.Md5HashGenerator;
 
 public class MainActivity extends BaseActivity {
     @Inject
@@ -17,5 +20,6 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         AndroidInjection.inject(this);
         setContentView(R.layout.activity_main);
+        Log.i("teste", Md5HashGenerator.generateMd5(Constants.PRIVATE_KEY + Constants.PUBLIC_KEY));
     }
 }
--- app/src/main/java/douglas/com/br/testemarvel/utils/helpers/Md5HashGenerator.java
+++ app/src/main/java/douglas/com/br/testemarvel/utils/helpers/Md5HashGenerator.java
@@ -1,8 +1,43 @@
 package douglas.com.br.testemarvel.utils.helpers;
 
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
 /**
  * Created by douglaspanacho on 30/11/2017.
+ * Class used to create the md5 hash value
  */
 
 public class Md5HashGenerator {
+
+    public static String generateMd5(String keys) {
+        String mTimeStamp = getTimeStamp();
+        Log.i("ts", mTimeStamp);
+        keys = mTimeStamp + keys;
+        try {
+            java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+            byte[] array = md.digest(keys.getBytes());
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < array.length; ++i) {
+                sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1, 3));
+            }
+            return sb.toString();
+        } catch (java.security.NoSuchAlgorithmException e) {
+        }
+        return null;
+
+    }
+
+    public static String getTimeStamp() {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.getDefault());
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+        return sdf.format(new Date());
+    }
 }
